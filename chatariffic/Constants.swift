8c36de7a63de465555f9382d120775703722312b
William L. Marr III <hobbyist@wlmiii.net>
2016-05-12 11:46:35 -0400

    Add custom class for UIView to create nice shadow effects

---
+++ chatariffic/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  chatariffic
+//
+//  Created by William L. Marr III on 5/12/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
