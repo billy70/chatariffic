da9707c8d90598411005cc111fb20ac2a48a0fda
William L. Marr III <hobbyist@wlmiii.net>
2016-05-14 13:59:08 -0400

    Authenticate user with Facebook
    Note that the authentication code for Firebase via Facebook is not implemented yet.

--- chatariffic/ViewController.swift
+++ chatariffic/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func fbButtonTapped(sender: UIButton!) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logInWithReadPermissions(["email"], fromViewController: self) { (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) -> Void in
+            
+            if facebookError != nil {
+                print("Facebook login failed. Error: \(facebookError)")
+            } else if facebookResult.isCancelled {
+                print("Facebook login cancelled.")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("Successfully logged in with Facebook. Token: \(accessToken)")
+            }
+        }
+    }
 }
-
