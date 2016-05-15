c253342b93721a4fa34984a3652ec3b9bf51aa61
William L. Marr III <hobbyist@wlmiii.net>
2016-05-15 13:01:46 -0400

    Authenticate Firebase user via email and password
    If the user did not previously exist in Firebase when the Signup/Login button is tapped, the user is created if it is valid; otherwise, any error condition is displayed via an alert.
    
    Also, once the user has successfully logged in for the first time using email/password login, the app will automatically log them in each time the app is started so that they do not have to keep logging in.

--- chatariffic/ViewController.swift
+++ chatariffic/ViewController.swift
@@ -13,6 +13,9 @@ import FBSDKLoginKit
 
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +25,7 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
         
         if NSUserDefaults.standardUserDefaults().valueForKey(FIREBASE_KEY_UID) != nil {
+            print("Performing segue from viewDidAppear().")
             self.performSegueWithIdentifier(SEGUE_ID_SHOW_CHAT, sender: nil)
         }
     }
@@ -29,6 +33,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func showErrorAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
     }
 
     @IBAction func fbButtonTapped(sender: UIButton!) {
@@ -58,6 +70,61 @@ class ViewController: UIViewController {
                     }
                 })
             }
+        }
+    }
+    
+    @IBAction func emailLoginTapped(sender: UIButton!) {
+        
+        if let email = emailField.text where email != "", let password = passwordField.text where password != "" {
+            
+            DataService.ds.REF_BASE.authUser(email, password: password) {
+                error, firebaseAuthData in
+                
+                if error != nil {
+                    
+                    print("Firebase authentication failure. Error code: \(error.code)")
+                    print("   Error: \(error)")
+
+                    if let errorCode = FAuthenticationError(rawValue: error.code) {
+                        switch (errorCode) {
+                            case .UserDoesNotExist:
+                                print("Handle invalid user")
+                            
+                                DataService.ds.REF_BASE.createUser(email, password: password, withValueCompletionBlock: { (error, result) in
+                                    if error != nil {
+                                        self.showErrorAlert("Could not create account", message: "Problem creating account. Please try again.")
+                                    } else {
+                                        NSUserDefaults.standardUserDefaults().setValue(result[FIREBASE_KEY_UID], forKey: FIREBASE_KEY_UID)
+                                        
+                                        DataService.ds.REF_BASE.authUser(email, password: password, withCompletionBlock: nil)
+                                        print("User logged in successfully.")
+                                        self.performSegueWithIdentifier(SEGUE_ID_SHOW_CHAT, sender: nil)
+                                    }
+                                })
+                            case .EmailTaken:
+                                print("Handle email already taken")
+                                self.showErrorAlert("Email address already taken", message: "The email address \(email) is already taken. Please use a different email address.")
+                            case .InvalidEmail:
+                                print("Handle invalid email")
+                                self.showErrorAlert("Email address is invalid", message: "The email address \(email) is invalid. Please use a valid email address.")
+                            case .InvalidPassword:
+                                print("Handle invalid password")
+                                self.showErrorAlert("Password invalid", message: "The password is invalid. Please use the correct password.")
+                            default:
+                                print("Handle default situation")
+                                self.showErrorAlert("An unknown error occurred.", message: "Please try again with different credentials, or try again later.")
+                        }
+                    }
+                    
+                } else {
+                    // User is logged into Firebase.
+                    print("User sucessfully logged into Firebase with email/password.")
+                    self.performSegueWithIdentifier(SEGUE_ID_SHOW_CHAT, sender: nil)
+                    
+                }
+            }
+        } else {
+            showErrorAlert("Email and password required", message: "You must enter an email and password to signup.")
         }
     }
 }
